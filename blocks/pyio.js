f87f8512e00a9c13786716594bf1cbb077d72495
Matthew Poole <Matthew.J.Poole@gmail.com>
2016-08-26 15:09:46 +0100

    Corrected type of format method

--- blocks/pyio.js
+++ blocks/pyio.js
@@ -182,7 +182,9 @@ Blockly.Blocks['python_format'] = {
         .appendField(")");
     this.setInputsInline(true);
     this.setTypeVecs([
-      ["str", "any", "str"]
+      ["str", "int", "str"],
+      ["str", "float", "str"],
+      ["str", "str", "str"]
     ]);
     this.setOutput(true);
     this.setTooltip('');
